5706aeb85be51e5fbc0567190ba59cb92bb0e01f
rukshan-prog <rukshanprog@gmail.com>
2025-05-13 02:04:03 +0530

    first commit

---
+++ src/main/java/com/rokshanprog/rokutask/Rokutask.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rokshanprog.rokutask;
+
+import com.rokshanprog.rokutask.ui.MainWindow;
+
+/**
+ *
+ * @author Rasintha_Rukshan
+ */
+public class Rokutask {
+
+    public static void main(String[] args) {
+        System.out.println("Application Start");
+        MainWindow.mainWindow();
+    }
+    
+}
+
+// color palet: https://colorhunt.co/palette/222831393e4600adb5eeeeee
